bc3b0f935f3648815f4d6b5a268e37aba3088b25
PythonSwiftLink <pythonswiftlink@gmail.com>
2024-12-15 23:18:08 +0100

    Update PackageTargets.swift

--- Sources/SwiftPackage/PackageTargets.swift
+++ Sources/SwiftPackage/PackageTargets.swift
@@ -31,12 +31,14 @@ public struct PackageTarget: PackageTargetProtocol {
     let name: String
     let dependencies: [any TargetDependency]
     let linker_settings: [LinkedSetting]
+    let resources: [Resource]
     
     public init(from decoder: any Decoder) throws {
         let c = try decoder.container(keyedBy: CodingKeys.self)
         name = try c.decode(forKey: .name)
         dependencies = try c.decode(forKey: .dependencies)
         linker_settings = try c.decode(forKey: .linker_settings)
+        resources = try c.decode(forKey: .resources)
     }
     
 
@@ -45,6 +47,7 @@ public struct PackageTarget: PackageTargetProtocol {
         case name
         case dependencies
         case linker_settings
+        case resources
     }
     
  
@@ -82,6 +85,25 @@ public extension PackageTarget {
         }
     }
     
+    struct Resource: Decodable {
+        enum Kind: String, Decodable {
+            case copy
+            case process
+        }
+        let kind: Kind
+        let path: String
+        
+        public var arrayElement: ArrayElementSyntax {
+            let expr: ExprSyntax = switch kind {
+            case .copy:
+                ".copy(\(literal: path))"
+            case .process:
+                ".process(\(literal: path))"
+            }
+            return .init(expression: expr)
+        }
+    }
+    
     var arrayElement: ArrayElementSyntax {
         let deps: ArrayElementListSyntax = .init {
             for dependency in self.dependencies {
@@ -93,10 +115,16 @@ public extension PackageTarget {
                 linker.arrayElement.with(\.leadingTrivia, .newline + .tabs(2))
             }
         }
+        let res: ArrayElementListSyntax = .init {
+            for resource in self.resources {
+                resource.arrayElement.with(\.leadingTrivia, .newline + .tabs(2))
+            }
+        }
         let expr: ExprSyntax = """
         .target(
             name: \(literal: name),
             dependencies: [\(deps)\n\t],
+            resources: [\(res)\n\t],
             linkerSettings: [\(linkers)\n\t]
         )
         """
